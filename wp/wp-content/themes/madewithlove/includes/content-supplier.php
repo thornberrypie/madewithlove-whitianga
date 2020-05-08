b828dc1ee1a95a0a39722bc4ad62464ff88d09c0
GraemeT <gdthornber@gmail.com>
2020-05-08 20:11:31 +1200

    Add form to supplier page

--- wp/wp-content/themes/madewithlove/includes/content-supplier.php
+++ wp/wp-content/themes/madewithlove/includes/content-supplier.php
@@ -3,5 +3,8 @@
   <div class="mwl-content-main">
     <h1 class="mwl-content-title"><?php the_title() ?></h1>
     <?php the_content() ?>
+    <div class="mwl-form">
+      <?php echo FrmFormsController::get_form_shortcode( array( 'id' => 1, 'title' => false ) ); ?>
+    </div>
   </div>
 </div>
